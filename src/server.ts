db676be356e61132937c5ac5024f353a71651682
evgshk <evgshk@gmail.com>
2024-06-16 11:28:09 +0100

    Fix deploy

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import bot from './bot';
 import express from 'express';
 
 const token = process.env.TELEGRAM_TOKEN;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
